0583a9167a6ef9b6b68bfda355b1a17a3cc77e31
Matt Travi <programmer@travi.org>
2018-01-19 00:23:55 -0600

    feat(update-notifier): added the notifier

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import program from 'commander';
+import updateNotifier from 'update-notifier';
 import pkg from '../package.json';
 
 program
   .version(pkg.version)
   .parse(process.argv);
+
+updateNotifier({pkg}).notify();
