0f1a1eaf3fa55167d6d477aa8416e134afbb95cf
Matt Travi <programmer@travi.org>
2018-02-10 23:48:31 -0600

    refactor(scaffold): stubbed out a scaffold sub-command
    no behavior yet tho

---
+++ src/scaffold-project/sub-command.js
@@ -0,0 +1,8 @@
+import scaffolder from './scaffolder';
+
+export default function (program) {
+  program
+    .command('scaffold')
+    .description('scaffold a new project')
+    .action(scaffolder)
+}
