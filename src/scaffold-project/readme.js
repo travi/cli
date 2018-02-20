b5e4bc2a38e010cd0d30cc5020d4a019f1b3639e
Matt Travi <programmer@travi.org>
2018-02-19 22:37:42 -0600

    refactor(readme): pulled badge and references up to generic lists
    this way more of the logic in code and the mustache template is just a dumb rendering of looping
    through the lists

--- src/scaffold-project/readme.js
+++ src/scaffold-project/readme.js
@@ -3,18 +3,31 @@ import {resolve} from 'path';
 import chalk from 'chalk';
 import mustache from 'mustache';
 
+function buildLicenseBadge(user, repoName) {
+  return `https://img.shields.io/github/license/${user}/${repoName}.svg`;
+}
+
 export default async function ({projectName, projectRoot, description, license, vcs}) {
   console.log(chalk.blue('Generating README'));     // eslint-disable-line no-console
 
-  const badgeData = {license: 'LICENSE'};
-  const licenseBadge = (vcs && 'GitHub' === vcs.host) && 'https://img.shields.io/github/license/travi/cli.svg';
+  const user = 'travi';
+  const repoName = 'cli';
+  const licenseBadge = (vcs && 'GitHub' === vcs.host) && buildLicenseBadge(user, repoName);
   const licenseAlt = (license && licenseBadge) && `${license} license`;
+  const badges = {
+    consumer: [(license && licenseBadge) && `[![${license} license][license-badge]][license]`].filter(Boolean)
+  };
+
+  const references = [
+    !!license && {key: 'license', value: 'LICENSE'},
+    !!(license && licenseBadge) && {key: 'license-badge', value: licenseBadge}
+  ].filter(Boolean);
 
   await writeFile(
     `${projectRoot}/README.md`,
     mustache.render(
       await readFile(resolve(__dirname, './templates/README.mustache'), 'utf8'),
-      {projectName, description, badgeData, license, licenseBadge, licenseAlt}
+      {projectName, description, references, badges, licenseAlt}
     )
   );
 }
