1a3dee1da52a60bef425c71d771e3da451b5f771
Matt Travi <programmer@travi.org>
2020-05-05 16:29:09 -0500

    build(rollup): handled the deprecation issue w/ the node-resolve plugin

--- rollup.config.js
+++ rollup.config.js
@@ -9,10 +9,7 @@ export default {
   input: 'src/index.js',
   plugins: [
     autoExternal(),
-    nodeResolve({
-      module: true,
-      jsnext: true
-    }),
+    nodeResolve({mainFields: ['module']}),
     json(),
     babel({
       babelrc: false,
