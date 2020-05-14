5454185e1419e72bec607af9f19df41afac5295a
Matt Travi <programmer@travi.org>
2020-05-14 16:43:59 -0500

    feat(lift): added the github-actions ci scaffolder for node projects

--- src/lift/action-test.js
+++ src/lift/action-test.js
@@ -3,6 +3,7 @@ import {scaffold as scaffoldRenovate} from '@form8ion/renovate-scaffolder';
 import {removeGreenkeeper} from '@form8ion/remove-greenkeeper';
 import {scaffold as scaffoldCucumber} from '@form8ion/cucumber-scaffolder';
 import {test as jsApplicabilityTest} from '@form8ion/lift-javascript';
+import {scaffold as scaffoldGithubActions} from '@form8ion/github-actions-node-ci';
 import sinon from 'sinon';
 import {assert} from 'chai';
 import any from '@travi/any';
@@ -27,7 +28,8 @@ suite('lift action', () => {
         scaffolders: {
           Renovate: scaffoldRenovate,
           'Remove Greenkeeper': removeGreenkeeper,
-          Cucumber: scaffoldCucumber
+          Cucumber: scaffoldCucumber,
+          'GitHub Actions CI': scaffoldGithubActions
         },
         enhancers: {JavaScript: {test: jsApplicabilityTest, lift: liftJavascript}}
       })
--- src/lift/action.js
+++ src/lift/action.js
@@ -3,6 +3,7 @@ import {scaffold as scaffoldRenovate} from '@form8ion/renovate-scaffolder';
 import {removeGreenkeeper} from '@form8ion/remove-greenkeeper';
 import {scaffold as scaffoldCucumber} from '@form8ion/cucumber-scaffolder';
 import {test as jsApplicabilityTest} from '@form8ion/lift-javascript';
+import {scaffold as scaffoldGithubActions} from '@form8ion/github-actions-node-ci';
 import {javascript as liftJavascript} from './enhanced-lifters';
 
 export default function () {
@@ -10,7 +11,8 @@ export default function () {
     scaffolders: {
       Renovate: scaffoldRenovate,
       'Remove Greenkeeper': removeGreenkeeper,
-      Cucumber: scaffoldCucumber
+      Cucumber: scaffoldCucumber,
+      'GitHub Actions CI': scaffoldGithubActions
     },
     enhancers: {JavaScript: {test: jsApplicabilityTest, lift: liftJavascript}}
   });
