e11d1fea5d97bb06a678c6f4a692764640786009
Matt Travi <programmer@travi.org>
2020-10-22 11:01:09 -0500

    feat(cypress): added the cypress scaffolder to lift

--- src/lift/action-test.js
+++ src/lift/action-test.js
@@ -2,6 +2,7 @@ import * as lifter from '@form8ion/lift';
 import {scaffold as scaffoldRenovate} from '@form8ion/renovate-scaffolder';
 import {removeGreenkeeper} from '@form8ion/remove-greenkeeper';
 import {scaffold as scaffoldCucumber} from '@form8ion/cucumber-scaffolder';
+import {scaffold as scaffoldCypress} from '@form8ion/cypress-scaffolder';
 import {test as jsApplicabilityTest} from '@form8ion/lift-javascript';
 import {scaffold as scaffoldGithubActions} from '@form8ion/github-actions-node-ci';
 import sinon from 'sinon';
@@ -31,6 +32,7 @@ suite('lift action', () => {
           'Remove Greenkeeper': removeGreenkeeper,
           'Unit Testing': unitTesting,
           Cucumber: scaffoldCucumber,
+          Cypress: scaffoldCypress,
           'GitHub Actions CI': scaffoldGithubActions
         },
         enhancers: {JavaScript: {test: jsApplicabilityTest, lift: liftJavascript}}
