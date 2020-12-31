828151d84a60cbb778cbe38c972f348914ed41ea
Matt Travi <programmer@travi.org>
2020-12-30 18:05:06 -0600

    feat(lift): wired up the GH Actions CI lifter

--- src/lift/action.js
+++ src/lift/action.js
@@ -4,7 +4,11 @@ import {removeGreenkeeper} from '@form8ion/remove-greenkeeper';
 import {scaffold as scaffoldCucumber} from '@form8ion/cucumber-scaffolder';
 import {scaffold as scaffoldCypress} from '@form8ion/cypress-scaffolder';
 import {test as jsApplicabilityTest} from '@form8ion/lift-javascript';
-import {scaffold as scaffoldGithubActions} from '@form8ion/github-actions-node-ci';
+import {
+  scaffold as scaffoldGithubActions,
+  lift as liftGithubActionsCI,
+  test as githubActionsCiApplicabilityTest
+} from '@form8ion/github-actions-node-ci';
 import {replace as replaceTravisCiWithGithubActions} from '@form8ion/replace-travis-ci-with-github-actions';
 import {javascript as liftJavascript} from './enhanced-lifters';
 import {unitTesting} from './enhanced-scaffolders';
@@ -20,6 +24,9 @@ export default function () {
       'GitHub Actions CI': scaffoldGithubActions,
       'Replace Travis CI with GitHub Actions': replaceTravisCiWithGithubActions
     },
-    enhancers: {JavaScript: {test: jsApplicabilityTest, lift: liftJavascript}}
+    enhancers: {
+      JavaScript: {test: jsApplicabilityTest, lift: liftJavascript},
+      'GitHub Actions CI': {test: githubActionsCiApplicabilityTest, lift: liftGithubActionsCI}
+    }
   });
 }
