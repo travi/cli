19f9204eb31c77d2432410c35b9ca830e3fa837b
Matt Travi <programmer@travi.org>
2021-06-24 01:10:46 -0500

    feat(add-package): enabled proper population of initial readme content for a monorepo package

--- test/integration/features/step_definitions/high-level/add-package-steps.js
+++ test/integration/features/step_definitions/high-level/add-package-steps.js
@@ -1,7 +1,20 @@
 import {promises as fs} from 'fs';
+import parse from 'mdast-util-from-markdown';
+import find from 'unist-util-find';
 import {Given, Then} from '@cucumber/cucumber';
 import td from 'testdouble';
 import {assert} from 'chai';
+
+async function assertReadmeDetailsArePopulatedProperly(projectName) {
+  const readmeTree = parse(await fs.readFile(`${process.cwd()}/packages/${projectName}/README.md`, 'utf-8'));
+
+  assert.isDefined(find(
+    readmeTree,
+    {type: 'heading', depth: 2, children: [{type: 'text', value: 'Table of Contents'}]}
+  ));
+  assert.isDefined(find(readmeTree, {type: 'heading', depth: 2, children: [{type: 'text', value: 'Usage'}]}));
+  assert.isDefined(find(readmeTree, {type: 'heading', depth: 2, children: [{type: 'text', value: 'Contributing'}]}));
+}
 
 Given('a lerna monorepo exists', async function () {
   this.monorepoType = 'lerna';
@@ -16,11 +29,12 @@ Given('a lerna monorepo exists', async function () {
 
 Then('the package will have repository details defined', async function () {
   assert.deepEqual(
-    JSON.parse(await fs.readFile(`${process.cwd()}/packages/${this.projectName}/package.json`)).repository,
+    JSON.parse(await fs.readFile(`${process.cwd()}/packages/${this.projectName}/package.json`, 'utf-8')).repository,
     {
       directory: `packages/${this.projectName}`,
       type: 'git',
       url: `https://github.com/${this.githubUser}/${this.projectName}.git`
     }
   );
+  await assertReadmeDetailsArePopulatedProperly(this.projectName);
 });
