9cf2a07e0405ec2ffad3c05ed0031881f2f443e8
Matt Travi <programmer@travi.org>
2022-11-10 17:01:52 -0600

    test(shell-scaffolder): adjusted the import to account for bundling adjustments

--- src/common/enhanced-scaffolders-test.js
+++ src/common/enhanced-scaffolders-test.js
@@ -1,5 +1,5 @@
 import * as javascriptPlugin from '@form8ion/javascript';
-import * as shellScaffolder from '@travi/shell-scaffolder';
+import shellScaffolder from '@travi/shell-scaffolder';
 import * as gitlabScaffolder from '@travi/gitlab-scaffolder';
 import {scaffold as scaffoldTravisForShell} from '@travi/travis-scaffolder-shell';
 import any from '@travi/any';
