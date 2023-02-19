bc41c3307cb938b220ecbc8addee617311e8f802
Matt Travi <programmer@travi.org>
2023-02-19 15:36:51 -0600

    test(unit): converted the enhanced-lifters test for the lift command to vitest

---
+++ src/lift/enhanced-lifters.test.js
@@ -0,0 +1,25 @@
+import * as jsLifter from '@form8ion/javascript';
+
+import {afterEach, beforeEach, describe, expect, it, vi} from 'vitest';
+import any from '@travi/any';
+import {when} from 'jest-when';
+
+import {javascript} from './enhanced-lifters';
+
+describe('enhanced lifters', () => {
+  beforeEach(() => {
+    vi.mock('@form8ion/javascript');
+  });
+
+  afterEach(() => {
+    vi.clearAllMocks();
+  });
+
+  it('should pass along the custom properties with the provided options to the js lifter', async () => {
+    const options = any.simpleObject();
+    const results = any.simpleObject();
+    when(jsLifter.lift).calledWith({...options, configs: {eslint: {scope: '@travi'}}}).mockResolvedValue(results);
+
+    expect(await javascript(options)).toEqual(results);
+  });
+});
