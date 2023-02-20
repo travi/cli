a0ab9105d2c400b3dfa16b1296309aeed10ab655
Matt Travi <programmer@travi.org>
2023-02-19 21:43:07 -0600

    test(unit): converted the common project options test to vitest

---
+++ src/common/project-options.test.js
@@ -0,0 +1,43 @@
+import {scaffold as scaffoldRuby} from '@form8ion/ruby-scaffolder';
+import {prompt as githubPrompt, scaffold as scaffoldGithub} from '@travi/github-scaffolder';
+import {scaffold as scaffoldGitlab} from '@travi/gitlab-scaffolder';
+import {scaffold as scaffoldDependabot} from '@form8ion/dependabot-scaffolder';
+import {scaffold as scaffoldRenovate} from '@form8ion/renovate-scaffolder';
+
+import {afterEach, beforeEach, describe, expect, it, vi} from 'vitest';
+import any from '@travi/any';
+import {when} from 'jest-when';
+
+import {defineScaffoldProjectOptions} from './project-options';
+import * as enhancedScaffolders from './enhanced-scaffolders';
+
+describe('common config', () => {
+  const decisions = any.simpleObject();
+
+  beforeEach(() => {
+    vi.mock('./enhanced-scaffolders');
+  });
+
+  afterEach(() => {
+    vi.clearAllMocks();
+  });
+
+  it('should define scaffold-project options', () => {
+    const jsScaffolder = () => undefined;
+    when(enhancedScaffolders.javascriptScaffolderFactory).calledWith(decisions).mockReturnValue(jsScaffolder);
+
+    expect(defineScaffoldProjectOptions(decisions)).toEqual({
+      languages: {JavaScript: jsScaffolder, Ruby: scaffoldRuby, Shell: enhancedScaffolders.shell},
+      vcsHosts: {
+        GitHub: {scaffolder: scaffoldGithub, prompt: githubPrompt, public: true, private: true},
+        GitLab: {scaffolder: scaffoldGitlab, prompt: enhancedScaffolders.gitlabPrompt, private: true}
+      },
+      dependencyUpdaters: {
+        Dependabot: {scaffolder: scaffoldDependabot},
+        Renovate: {scaffolder: scaffoldRenovate}
+      },
+      overrides: {copyrightHolder: 'Matt Travi'},
+      decisions
+    });
+  });
+});
