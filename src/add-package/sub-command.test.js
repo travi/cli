aaa8f0d0a0f1000c6d798406bd641bf210edb650
Matt Travi <programmer@travi.org>
2023-03-17 16:32:04 +0100

    test(unit): converted the add-package sub-command to vitest

---
+++ src/add-package/sub-command.test.js
@@ -0,0 +1,19 @@
+import {describe, expect, it, vi} from 'vitest';
+import {when} from 'jest-when';
+
+import addPackageAction from './action';
+import addPackageCommand from './sub-command';
+
+describe('add-package sub-command', () => {
+  it('should make the add-package command available', async () => {
+    const command = vi.fn();
+    const description = vi.fn();
+    const action = vi.fn();
+    when(command).calledWith('add-package').mockReturnValue({description});
+    when(description).calledWith('Add a JavaScript package to an existing monorepo').mockReturnValue({action});
+
+    addPackageCommand({command});
+
+    expect(action).toHaveBeenCalledWith(addPackageAction);
+  });
+});
