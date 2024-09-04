652ebcc93299297e0911c2d5698f41bb79a0ede5
Matt Travi <programmer@travi.org>
2024-09-03 22:26:28 -0500

    fix(js-enhancers): upgroup the plugins to be used as enhancers

--- src/common/enhanced-lifters.js
+++ src/common/enhanced-lifters.js
@@ -1,10 +1,12 @@
+import {ungroupObject} from '@form8ion/core';
 import {lift} from '@form8ion/javascript';
+
 import {plugins} from './javascript-options.js';
 
 export function javascript(options) {
   return lift({
     ...options,
     configs: {eslint: {scope: '@travi'}},
-    enhancers: plugins()
+    enhancers: ungroupObject(plugins())
   });
 }
--- src/common/enhanced-lifters.test.js
+++ src/common/enhanced-lifters.test.js
@@ -1,3 +1,4 @@
+import {ungroupObject} from '@form8ion/core';
 import * as jsLifter from '@form8ion/javascript';
 
 import {afterEach, beforeEach, describe, expect, it, vi} from 'vitest';
@@ -6,6 +7,9 @@ import {when} from 'jest-when';
 
 import {plugins} from './javascript-options.js';
 import {javascript} from './enhanced-lifters.js';
+
+vi.mock('@form8ion/core');
+vi.mock('./javascript-options.js');
 
 describe('enhanced lifters', () => {
   beforeEach(() => {
@@ -19,12 +23,12 @@ describe('enhanced lifters', () => {
   it('should pass along the custom properties with the provided options to the js lifter', async () => {
     const options = any.simpleObject();
     const results = any.simpleObject();
+    const groupedPlugins = any.simpleObject();
+    const ungroupedPlugins = any.simpleObject();
+    plugins.mockReturnValue(groupedPlugins);
+    when(ungroupObject).calledWith(groupedPlugins).mockReturnValue(ungroupedPlugins);
     when(jsLifter.lift)
-      .calledWith({
-        ...options,
-        configs: {eslint: {scope: '@travi'}},
-        enhancers: plugins()
-      })
+      .calledWith({...options, configs: {eslint: {scope: '@travi'}}, enhancers: ungroupedPlugins})
       .mockResolvedValue(results);
 
     expect(await javascript(options)).toEqual(results);
